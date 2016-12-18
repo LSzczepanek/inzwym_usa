2f1246b2fd64fb433f0f9f2ee6e39e27ff8a172c
rafal <roen00@gmail.com>
2016-12-18 16:39:34 +0100

    added dummy csv reader

--- src/main/java/taxes/app/App.java
+++ src/main/java/taxes/app/App.java
@@ -1,13 +1,28 @@
 package taxes.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileWriter;
+import java.io.IOException;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new App();
     }
+    
+    public App(){
+
+        FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter("data.csv", true);
+			CSVWriter writer = new CSVWriter(fileWriter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
 }
