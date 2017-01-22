03a12f3c80b6de8cff05279d2801dd60e97d761c
Rafal Zelek <r.zelek@oberthur.com>
2017-01-22 17:23:39 +0100

    added categories to data model

--- src/main/java/taxes/app/view/MainView.java
+++ src/main/java/taxes/app/view/MainView.java
@@ -9,6 +9,7 @@ public class MainView extends JFrame {
 
 	private DataModel dataModel;
 	private JComboBox<String> states = new JComboBox<>();
+	private JComboBox<String> categories = new JComboBox<>();
 
 	public MainView(DataModel dataModel) {
 		super("Hello World");
@@ -17,6 +18,8 @@ public class MainView extends JFrame {
 		setSize(500, 500);
 		setLocation(50,50);
 		setLayout(new GridBagLayout());
+		add(categories);
+		dataModel.getCategories().forEach(state -> categories.addItem(state));
 		add(states);
 		dataModel.getStates().forEach(state -> states.addItem(state));
 		setVisible(true);
