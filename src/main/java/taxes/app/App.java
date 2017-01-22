3872ef87140d79ba7539908289077fed2c165366
Rafal Zelek <r.zelek@oberthur.com>
2017-01-22 17:11:34 +0100

    removed unused csv reader

--- src/main/java/taxes/app/App.java
+++ src/main/java/taxes/app/App.java
@@ -1,32 +1,20 @@
 package taxes.app;
 
-import au.com.bytecode.opencsv.CSVReader;
-import au.com.bytecode.opencsv.bean.CsvToBean;
 import taxes.app.view.MainView;
 
 import java.awt.*;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> new MainView(new DataModel() {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> new MainView(new DataModel() {
 
-			@Override
-			public List<String> getStates() {
-				return Arrays.asList("state1", "state2");
-			}
-		}));
-
-		try {
-		     CSVReader csvReader = new CSVReader(new FileReader("StanyZPodatkami.csv"));
-
-		      CsvToBean csv = new CsvToBean();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            @Override
+            public List<String> getStates() {
+                return Arrays.asList("state1", "state2");
+            }
+        }));
+    }
 }
